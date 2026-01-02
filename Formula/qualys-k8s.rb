a1218e7515bf3956f677a35ab8e73b6aa338d5e2
Andrew Nelson <nelson84.03@gmail.com>
2026-01-02 13:13:21 -0800

    Update to 0.1.3

--- Formula/qualys-k8s.rb
+++ Formula/qualys-k8s.rb
@@ -2,19 +2,19 @@ class QualysK8s < Formula
   desc "Agentless Kubernetes security scanner for CIS, NSA-CISA, and MITRE compliance"
   homepage "https://github.com/nelssec/qualys-agentless"
   license "Apache-2.0"
-  version "0.1.2"
+  version "0.1.3"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/nelssec/qualys-agentless/releases/download/v#{version}/qualys-k8s-darwin-arm64"
-      sha256 "cad2958076329a0d9f3be1c0ce4c6318f6edd4da20c45a6d79cb87c68347d511"
+      sha256 "3549dc35d133bb5e921f57baa13da5c6936d92f3a1a6183605799bfe7fbfe071"
 
       def install
         bin.install "qualys-k8s-darwin-arm64" => "qualys-k8s"
       end
     else
       url "https://github.com/nelssec/qualys-agentless/releases/download/v#{version}/qualys-k8s-darwin-amd64"
-      sha256 "45e0f465f6db629ea9607d70bd673f2dea21a7900fd3c5c4fc321cd522f5d7b4"
+      sha256 "95fdda03ff4b84f61ae41e7b6d18db27cacc5f9fb2e41c2f06ac4d62c7ce3e8b"
 
       def install
         bin.install "qualys-k8s-darwin-amd64" => "qualys-k8s"
@@ -25,14 +25,14 @@ class QualysK8s < Formula
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/nelssec/qualys-agentless/releases/download/v#{version}/qualys-k8s-linux-arm64"
-      sha256 "0b8c07c7851faacbf241261b6277aef63299d451dda209a09945b97036691c4b"
+      sha256 "8ef2ea30d8673c234e47bcb88a5e1d925885eaa6af67dd5e7c6d934574ec9e4e"
 
       def install
         bin.install "qualys-k8s-linux-arm64" => "qualys-k8s"
       end
     else
       url "https://github.com/nelssec/qualys-agentless/releases/download/v#{version}/qualys-k8s-linux-amd64"
-      sha256 "60b830027580fe6163226117eeb38351b773b442e3b078513837bb9952ac1050"
+      sha256 "9d92e5424488ec79f0b5c4656d5ad6477e8a2a14a5fa5fc6f77c2ec084649f9c"
 
       def install
         bin.install "qualys-k8s-linux-amd64" => "qualys-k8s"
