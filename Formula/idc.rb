0b112779888bf8781650da55f81cb61d18098fc5
Andrew Nelson <nelson84.03@gmail.com>
2026-01-04 21:33:59 -0800

    Updates

--- Formula/idc.rb
+++ Formula/idc.rb
@@ -2,19 +2,19 @@ class Idc < Formula
   desc "Kubernetes Identity & Blast Radius Analyzer - maps RBAC to cloud IAM"
   homepage "https://github.com/nelssec/identity-chain"
   license "Apache-2.0"
-  version "0.1.2"
+  version "0.3.1"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/nelssec/identity-chain/releases/download/v#{version}/idc-darwin-arm64"
-      sha256 "9cede06e7b78fbd4d8f41711312789bd1db993ece73de7efd148e0ade26760a5"
+      sha256 "1fb4ce51486a87a77bed1298007ceea3331ecd119f1e35602a0bc9ca071e323b"
 
       def install
         bin.install "idc-darwin-arm64" => "idc"
       end
     else
       url "https://github.com/nelssec/identity-chain/releases/download/v#{version}/idc-darwin-amd64"
-      sha256 "21605fa7087f67a28e0e60187fe989d6e32ef97097f411b43190b5525aa50b71"
+      sha256 "af6faf5d6654658c9d2a3b541a64b54278636efcd665abd8a5b68ef3ee45324b"
 
       def install
         bin.install "idc-darwin-amd64" => "idc"
@@ -25,14 +25,14 @@ class Idc < Formula
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/nelssec/identity-chain/releases/download/v#{version}/idc-linux-arm64"
-      sha256 "929ad54f3d71ef98c9fcd85d19bf43c9b962d879a05954b42ea772a1f7c02e5b"
+      sha256 "bebe518a004eedf931f3d0563a7bb392bb2bfb1e3660ec31b0fcadf80711d893"
 
       def install
         bin.install "idc-linux-arm64" => "idc"
       end
     else
       url "https://github.com/nelssec/identity-chain/releases/download/v#{version}/idc-linux-amd64"
-      sha256 "ae20113bfe8dc077e3f3d04ac1d6c9227e8decd2a47299b0ea14d94efa5de3ce"
+      sha256 "8e2e237f89e272e374b7e4242d83fbd5d76b06c62c11aee53d451981ffc24c8d"
 
       def install
         bin.install "idc-linux-amd64" => "idc"
